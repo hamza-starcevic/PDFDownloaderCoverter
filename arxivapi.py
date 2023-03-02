699e857117c4506b933cdb7322cc86d0f41221a6
Hamza Starcevic <starcevic.hamza.21@size.ba>
2023-03-02 17:43:42 +0100

    Try except implemented

--- arxivapi.py
+++ arxivapi.py
@@ -11,11 +11,15 @@ search = arxiv.Search(
 # Add the pdf_url to the links array
 links = []
 for result in search.results():
-    links.append(result.pdf_url)
-    #Check if the pdf_url None or empty
-    if result.pdf_url is None or result.pdf_url == '':
-        print("No pdf_url for " + result.title)
-        continue
-    result.download_pdf(dirpath='pdfs', filename=result.title + '.pdf')
-    #Show how many have been downloaded out of the max_results
-    print("Downloaded " + str(len(links)) + " out of " + str(search.max_results))
+    try:
+      links.append(result.pdf_url)
+      #Check if the pdf_url None or empty
+      if result.pdf_url is None or result.pdf_url == '':
+          print("No pdf_url for " + result.title)
+          continue
+      result.download_pdf(dirpath='pdfs', filename=result.title + '.pdf')
+      #Show how many have been downloaded out of the max_results
+      print("Downloaded " + str(len(links)) + " out of " + str(search.max_results))
+    except:
+      print("Error downloading " + result.title)
+      continue
